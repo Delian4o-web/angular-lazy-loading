9b3ce3918ff441393a41da5a8a150730b8bfb112
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-08-26 12:13:03 +0200

    Add Lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'contact-us',
+    loadChildren: () => import('src/app/contact-us/contact-us.module').then(m => m.ContactUsModule)
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('src/app/about-us/about-us.module').then(m => m.AboutUsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
